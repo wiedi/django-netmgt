8b1d03c5bf7375829125b74e2bc73b8480d2e822
Thomas Merkel <tm@core.io>
2014-10-18 17:25:11 +0200

    Switch from django bootstrap to django suit
    Django Suit is much nicer than django bootstrap. The header needs to be set in the configuration file now.

--- netmgt_web/settings.py
+++ netmgt_web/settings.py
@@ -1,4 +1,6 @@
 import os
+from django.conf.global_settings import TEMPLATE_CONTEXT_PROCESSORS as TCP
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 SECRET_KEY = 'sb$z85@ix+!ur1wcl8ua)4-#iqr*5&)r$!+w@#r)zw&ifb1x=#'
@@ -7,8 +9,7 @@ TEMPLATE_DEBUG = True
 ALLOWED_HOSTS = []
 
 INSTALLED_APPS = (
-	'django_admin_bootstrapped.bootstrap3',
-	'django_admin_bootstrapped',
+	'suit',
 	'django.contrib.admin',
 	'django.contrib.auth',
 	'django.contrib.contenttypes',
@@ -27,6 +28,14 @@ MIDDLEWARE_CLASSES = (
 	'django.contrib.messages.middleware.MessageMiddleware',
 	'django.middleware.clickjacking.XFrameOptionsMiddleware',
 )
+
+TEMPLATE_CONTEXT_PROCESSORS = TCP + (
+	'django.core.context_processors.request',
+)
+
+SUIT_CONFIG = {
+	'ADMIN_NAME': 'Network Management Tool',
+}
 
 ROOT_URLCONF = 'netmgt_web.urls'
 WSGI_APPLICATION = 'netmgt_web.wsgi.application'
