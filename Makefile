cef71595ad919bbefe1e8a997a136b05afbb3ff4
Sebastian Wiedenroth <wiedi@frubar.net>
2024-11-02 21:51:21 +0100

    add openapi schema

--- Makefile
+++ Makefile
@@ -11,3 +11,6 @@ app-shell:
 	docker exec -it netmgt-app-1 bash
 django-shell:
 	docker exec -it netmgt-app-1 ./manage.py shell
+
+schema:
+	docker exec -it netmgt-app-1 bash -c './manage.py spectacular --file docs/openapi-schema.yml --validate'
--- netmgt_web/settings.py
+++ netmgt_web/settings.py
@@ -14,6 +14,7 @@ INSTALLED_APPS = (
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
 	'rest_framework',
+	'drf_spectacular',
 	'netmgt',
 )
 
@@ -75,7 +76,8 @@ REST_FRAMEWORK = {
 	'DEFAULT_RENDERER_CLASSES': [
 		'rest_framework.renderers.JSONRenderer',
 		'rest_framework.renderers.BrowsableAPIRenderer',
-	]
+	],
+	'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 
